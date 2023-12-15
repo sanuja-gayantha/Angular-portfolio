98ddd872601f7cdf97b40f182d22ace47b38a43a
sanuja <rpsgayantha@gmail.com>
2023-12-15 15:12:44 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      fontFamily: { 
-        'megrim': ['Megrim','system-ui','sans-serif'],
-        'fahkwang': ['Fahkwang','system-ui','sans-serif'] 
+      fontFamily: {
+        megrim: ["Megrim", "system-ui", "sans-serif"],
+        fahkwang: ["Fahkwang", "system-ui", "sans-serif"],
+      },
+      grayscale: {
+        10: "10%",
+        20: "20%",
+        30: "30%",
+        35: "35%",
+        40: "40%",
+        50: "50%",
+        60: "60%",
+        70: "70%",
+        80: "80%",
+        90: "90%",
+      },
     },
-    
-    },
-    
   },
   plugins: [],
-}
-
+};
