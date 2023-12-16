c08f2549116f3cf9fa0716ffe855df8b9bc6456b
sanuja <rpsgayantha@gmail.com>
2023-12-16 14:44:43 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { Page404Component } from './page404/page404.component';
 
 export const routes: Routes = [
-    {path: '**', component:Page404Component}
+    {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
