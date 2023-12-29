298bc8dffb83e482d05119b283b0330a4574c826
sanuja <rpsgayantha@gmail.com>
2023-12-30 00:53:27 +0530

    commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+
+
 @Component({
   selector: 'app-contact',
   standalone: true,
@@ -10,4 +13,14 @@ import { CommonModule } from '@angular/common';
 })
 export class ContactComponent {
   Year: number = new Date().getFullYear();
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target as HTMLFormElement, 'YOUR_PUBLIC_KEY')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 }
--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -36,12 +36,12 @@ export class ProjectsService {
     },
     {
       projectID: 2,
-      projectName: 'Scooters',
-      imageUrl: 'https://i.ibb.co/pv4G2Xj/Electric-cooters.webp',
-      githubUrl: 'https://github.com/sanuja-gayantha/Electric-scooters',
-      demoUrl: 'https://electric-scooters.netlify.app/',
-      desc1: 'Did you know that the first patent for an electric bicycle, a precursor to modern electric scooters, was filed in the late 19th century by Hosea W. Libbey?',
-      desc2: 'This digital storefront showcases an extensive range of cutting-edge electric scooters, along with an assortment of top-tier accessories meticulously curated for an enhanced riding experience.',
+      projectName: 'EShop',
+      imageUrl: 'https://i.ibb.co/LQGkdFB/Screenshot-2023-12-30-at-00-33-26-ESHOP.webp',
+      githubUrl: 'https://github.com/sanuja-gayantha/Ecommerce-fashion',
+      demoUrl: 'https://eshop-ecommerce-fashion.netlify.app/',
+      desc1: 'Did you know? The first online retail transaction occurred in 1994 when a Sting CD was sold on NetMarket, marking the inception of ecommerce in the fashion industry.',
+      desc2: 'This fashion ecommerce website 🛍️ provides a seamless shopping experience, allowing customers to effortlessly explore and buy a wide range of trendy products.',
       techStack: [
         {
           stackName: 'Angular',
@@ -63,12 +63,12 @@ export class ProjectsService {
     },
     {
       projectID: 3,
-      projectName: 'EShop',
-      imageUrl: 'https://i.ibb.co/ThsJ1kr/E-shop.webp',
-      githubUrl: 'https://github.com/sanuja-gayantha/Ecommerce-fashion',
+      projectName: 'Scooters',
+      imageUrl: 'https://i.ibb.co/pv4G2Xj/Electric-cooters.webp',
+      githubUrl: 'https://github.com/sanuja-gayantha/Electric-scooters',
       demoUrl: 'https://electric-scooters.netlify.app/',
-      desc1: 'Did you know that the fortune cookie, often associated with Chinese cuisine, actually originated in California, not in China?',
-      desc2: 'This is a small business website that sells snacks and cookies. customers can order their favorite flavors online.',
+      desc1: 'Did you know that the first patent for an electric bicycle, a precursor to modern electric scooters, was filed in the late 19th century by Hosea W. Libbey?',
+      desc2: 'This digital storefront showcases an extensive range of cutting-edge electric scooters, along with an assortment of top-tier accessories meticulously curated for an enhanced riding experience.',
       techStack: [
         {
           stackName: 'Angular',
@@ -81,10 +81,6 @@ export class ProjectsService {
         {
           stackName: 'Tailwindcss',
           stackUrl: '../../assets/images/tailwindcss.svg'
-        },
-        {
-          stackName: 'CSS3',
-          stackUrl: '../../assets/images/css3.svg'
         }
       ]
     },
